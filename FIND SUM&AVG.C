2f9cf53ad92bf875fbf86432d3482e43caff9938
aniruddhsingh2197 <115931991+aniruddhsingh2197@users.noreply.github.com>
2022-10-16 19:19:53 +0530

    Update FIND SUM&AVG.C

--- FIND SUM&AVG.C
+++ FIND SUM&AVG.C
@@ -2,11 +2,11 @@
 int main()
 {
   int n1 ,n2 ,n3 ,n4 ,n5 ,sum,avg;
-  printf("enter 5 marks:n1,n2,n3,n4,n5");
-  scanf("%d,%d,%d,%d,%d",&n1 ,&n2 ,&n3 ,&n4 ,&n5);
-  sum=n1+n2+n3+n4+n5;
+  printf("enter 5 marks:n1,n2,n3,n4,n5\n");
+  scanf("%d%d%d%d%d",&n1 ,&n2 ,&n3 ,&n4 ,&n5);
+  sum = n1+n2+n3+n4+n5;
   avg = sum/5;
-  printf("sum = %d\n"; sum);
-  printf("Avg = %d\n"; avg);
+  printf("sum = %d\n", sum);
+  printf("Avg = %d\n", avg);
   return 0 ;
 }
